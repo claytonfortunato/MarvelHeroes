7ca346ef9ec7e841986a88a61ae0247f7b225566
claytonfortunato <claytonreis10@gmail.com>
2023-05-15 21:22:02 -0300

    feat: environment variables

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,14 +1,13 @@
-import { Route, Routes, Router } from "react-router-dom";
+import { BrowserRouter as Route, Routes, Router } from "react-router-dom";
 
 import { Character } from "../pages/Character";
 import { Comics } from "../pages/Comics";
-import { Header } from "../layout/Header";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" />
+        <Route />
 
         <Route path="/characters" element={<Character />} />
         <Route path="/comics" element={<Comics />} />
--- src/services/api.ts
+++ src/services/api.ts
@@ -3,16 +3,17 @@ import md5 from "md5";
 
 const ts = Number(new Date());
 
-const publicKey = "9bef221ea2c9e669f3457b628732f017";
-const privateKey = "9284aa0ab2a2e5fb907b1e0b82cd3032d92420a7";
+const ApiKey = import.meta.env.VITE_KEYPUBLIC;
 
-const hash = md5(ts + privateKey + publicKey);
+const privateKey = import.meta.env.VITE_APP_KEYPRIVATE;
+
+const hash = md5(ts + privateKey + ApiKey);
 
 const api = axios.create({
   baseURL: "http://gateway.marvel.com/v1/public",
   params: {
     ts,
-    apikey: publicKey,
+    apikey: ApiKey,
     hash,
   },
 });
