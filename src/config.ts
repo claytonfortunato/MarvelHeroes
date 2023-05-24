67f95975d4eb52488a0ff77bc83a6c1ba3118971
claytonfortunato <claytonreis10@gmail.com>
2023-05-24 14:59:30 -0300

    docs: ajustes deploy

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,5 @@
 export default {
-  apiUrl:
-    import.meta.env.VITE_APP_API || "https://gateway.marvel.com:443/v1/public",
-  apiKey:
-    import.meta.env.VITE_APP_KEYPUBLIC || "9bef221ea2c9e669f3457b628732f017",
-  keyPrivate:
-    import.meta.env.VITE_APP_KEYPRIVATE ||
-    "9284aa0ab2a2e5fb907b1e0b82cd3032d92420a7",
+  apiUrl: import.meta.env.VITE_APP_API,
+  apiKey: import.meta.env.VITE_APP_KEYPUBLIC,
+  keyPrivate: import.meta.env.VITE_APP_KEYPRIVATE,
 };
